e60a42be85d753b771e39af4a59fd8ef4a564c1c
alex <alex.grich84@gmail.com>
2025-09-22 21:11:46 +0300

    lesson09

--- samur/src/App.tsx
+++ samur/src/App.tsx
@@ -1,30 +1,53 @@
+import { useState } from "react";
+
+
+// const tracks = null;
+const tracks = [
+	{id:1, title:'Musicfun soundtrek!',url:' https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3'},
+	{id:2,  title:'Musicfun soundtrek instrumental',url:'https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3'},
+	
+]
 
 
 
  export function App () {	
 
-const tracks = [
-	{id:1, title:'Musicfun soundtrek!',url:' https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3'},
-	{id:2, title:'Musicfun soundtrek instrumental',url:'https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3'},
-	{id:3, title:'Musicfun soundtrek!',url:' https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3'},
-	{id:4, title:'Musicfun soundtrek instrumental',url:'https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3'},
-]
+const [selectedTrackId, setSelectedTracId] = useState(null)
 
+
+	 if (tracks === null) {
+    return (
+      <div>
+        <h1>Musicfun player</h1>
+        <span>Loading...</span>
+      </div>
+    )
+  }
+
+ if (tracks.length === 0) {
+    return (
+      <div>
+        <h1>Musicfun player</h1>
+        <span>No tracks</span>
+      </div>
+    )
+  }
 
   return (
     <div>
-     
-	 <div>
 			<h1> Musicfun Player </h1>
-	</div>
-
+			<button onClick={()=>{
+				 setSelectedTracId(null)
+			}}>reset selection</button>
 		<ul>
 			 {
 				 tracks.map((track)=> {
   return (
-		<li key={track.id}>
-				<div>
-              	{track.title}
+		<li key={track.id} style={{border:track.id === selectedTrackId  ? '1px solid red': 'none'}}>
+				<div onClick={()=> {
+					 setSelectedTracId(track.id)
+				} }>
+              {track.title}
 				</div>
           <audio src={track.url} controls></audio>
 			</li>
